a94454877ee33e5acd6a351ea1f54f6a6d182b55
Antoine Rivoal <antoine.rivoal@silabs.com>
2024-01-11 11:12:14 +0100

    use cmake generator expression to retriev binary name inside function

--- proprietary.cmake
+++ proprietary.cmake
@@ -1,5 +1,6 @@
-function(app_post_build_func TARGET FILENAME)
+function(app_post_build_func TARGET)
 add_custom_command(TARGET ${TARGET} POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E echo "function app_post_build Triggered with parameter "
+    COMMAND ${CMAKE_COMMAND} -E echo "function app_post_build Triggered with parameter ${TARGET}"
+    COMMAND ${CMAKE_COMMAND} -E echo "output file is $<TARGET_FILE:hello>"
 )
 endfunction()
